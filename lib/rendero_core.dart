4e7f6c86409c1729518e01e00d827f8aaf5e01c6
vitalii katorgin <vitalykatorgin@gmail.com>
2023-10-21 11:57:36 -0300

    ok

---
+++ lib/rendero_core.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library rendero_core;
+
+export 'src/rendero_core_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ lib/src/rendero_core_base.dart
@@ -0,0 +1,22 @@
+typedef MSD = Map<String, dynamic>;
+
+T? cast<T>(x) => x is T ? x : null;
+
+T render<T>(MSD plugins, MSD j) {
+  if (plugins[j["module"]] == null) {
+    throw Exception("RENDERO ERROR: No have module ${j["module"]}");
+  }
+
+  if (plugins[j["module"]][j["type"]] == null) {
+    throw Exception(
+        "RENDERO ERROR: No have type ${j["type"]} in ${j["module"]}");
+  }
+
+  return plugins[j["module"]][j["type"]](
+    j["props"],
+    j["children"],
+    plugins,
+    render,
+  );
+}
+
